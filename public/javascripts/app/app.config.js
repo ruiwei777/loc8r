9ed791a6ec0c1e78d813926d90c8842a57709274
Ruiwei Liang <liangruiwei777@gmail.com>
2017-11-10 11:19:11 +1100

    set header  when requesting templates

--- public/javascripts/app/app.config.js
+++ public/javascripts/app/app.config.js
@@ -1,63 +1,61 @@
-(function(){
+(function () {
 	angular.module('loc8r')
-	.config(['$stateProvider','$locationProvider','$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider){
-		var locationListState = {
-			name: "locations",
-			url: "/locations",
-			component: "locationList",
-			resolve: {
-				locations: function(getLocations){
-					return getLocations();
+		.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', '$templateRequestProvider', function ($stateProvider, $locationProvider, $urlRouterProvider, $templateRequestProvider) {
+			var locationListState = {
+				name: "locations",
+				url: "/locations",
+				component: "locationList",
+				resolve: {
+					locations: function (getLocations) {
+						return getLocations();
+					}
 				}
 			}
-		}
 
-		var locationDetailState = {
-			name: "locationDetail",
-			url: "/locations/{locationId}",
-			component: "locationDetail",
-			params: {
-				location: null
+			var locationDetailState = {
+				name: "locationDetail",
+				url: "/locations/{locationId}",
+				component: "locationDetail",
+				params: {
+					location: null
+				}
 			}
-		}
 
-		var locationCreateState = {
-			name: "locationCreate",
-			url: "/locations/create",
-			component: "locationCreate"
-		}
+			var locationCreateState = {
+				name: "locationCreate",
+				url: "/locations/create",
+				component: "locationCreate"
+			}
 
-		var aboutState = {
-			name: "about",
-			url: "/about",
-			component: "about"
-		}
-
+			var aboutState = {
+				name: "about",
+				url: "/about",
+				component: "about"
+			}
 
 
 
-		
-		$stateProvider
-			.state(locationListState)
-			.state(locationDetailState)
-			.state(locationCreateState)
-			.state(aboutState);
 
-		$locationProvider.html5Mode(true);
-		
-		// index redirect
-		$urlRouterProvider.when("/", ['$state','$match', function ($state, $match) {
-			$state.go('locations');
 
+			$stateProvider
+				.state(locationListState)
+				.state(locationDetailState)
+				.state(locationCreateState)
+				.state(aboutState);
+
+			$locationProvider.html5Mode(true);
+
+			// index redirect
+			$urlRouterProvider.when("/", ['$state', '$match', function ($state, $match) {
+				$state.go('locations');
+
+			}]);
+
+			// set header when requesting templates for ui-router
+			$templateRequestProvider.httpOptions({
+				headers: { Accept: 'text/html' }
+			});
 		}]);
-	}]);
-
-
-
-
-
-
-	
 })();
 
 
